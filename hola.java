7067bc8a3747df1fb528114681c4ebd937fab2ed
Benjamín <pizarrochb3@gmail.com>
2025-04-30 19:32:33 -0600

    Se fue todo

--- hola.java
+++ hola.java
@@ -1,18 +1,1 @@
-// lab de  hilos. 
-public class hola extends Thread {
-    public void run() {
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Hola " + i);
-            try {
-                sleep(1000); // dormir un segundo
-            } catch (InterruptedException e) {
-                System.out.println("Error: " + e.getMessage());
-            }
-        }
-    }
-
-    public static void main(String[] args) {
-        hola h = new hola();
-        h.start(); // iniciar el hilo
-    }
-}
+// Borré todo lo que había escrito
